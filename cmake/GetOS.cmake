30b180bab3ad3a8efe3b6360b32d269595a0bad1
Saman Ghannadzadeh <saman@saman-gh.co.uk>
2025-04-15 15:39:52 +0100

    fixup! Fix `MSYS2` variable

--- cmake/GetOS.cmake
+++ cmake/GetOS.cmake
@@ -1,4 +1,4 @@
 # Note: the MSYS variable provided by CMAKE is difference
-if (${CMAKE_SYSTEM_NAME} MATCHES "MSYS")
-    set(MSYS2 TRUE)
+if (MSYS OR ${CMAKE_SYSTEM_NAME} MATCHES "MSYS")
+  set(MSYS2 TRUE)
 endif()
